776e815c1ffd202b3ddf212867bc2f664e7fe7a8
rolandexplore93 <roland2rule@gmail.com>
2022-05-24 00:45:44 +0100

    Set up nodejs environment

---
+++ test.js
@@ -0,0 +1,10 @@
+const name = "Roland";
+
+console.log(name)
+
+const greetings = (username) => {
+    console.log(`Hello ${username}, Welcome to Nodejs`)
+}
+
+greetings("Roland")
+greetings("Orobola")
