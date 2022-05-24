956ebaf775e113a656102a7a294b9b5d90c617a3
rolandexplore93 <roland2rule@gmail.com>
2022-05-24 01:29:31 +0100

    Learn import and export of external files, os - operating system features in node

--- modules.js
+++ modules.js
@@ -1,1 +1,18 @@
 // MODULES and REQUIRE
+// Multiples files usage and how to access them
+// use require() statement to import external files into current file
+const listOfPeople = require('./people')  //require() return an empty object
+const {people, ages} = require('./people')  //destructing exports
+
+// console.log(listOfPeople.people, listOfPeople.ages)
+// console.log(people, ages)
+
+// NOTE
+// 1. Importing an external file do not give you access to the file content
+// 2a. To access an external file content from your current file, add module.exports = 'value you want to be displayed' in the external file
+// this value of module.export will be accessible
+
+
+// Modules in nodejs- OS modules
+const os = require('os')
+console.log(os.platform(), os.homedir())
---
+++ people.js
@@ -0,0 +1,12 @@
+const people = ["Roland", "Yooshi", "Batty", "Messi"]
+const ages = [29, 21, 25, 43]
+// console.log(people)
+
+// module.exports = people
+// module.exports = "Testing exports"
+
+//MULTIPLE EXPORTS
+module.exports = {
+    people, 
+    ages
+}
