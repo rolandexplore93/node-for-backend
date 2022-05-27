0be8992bb9a7ee21be8cc202c366b0032a3b6783
rolandexplore93 <roland2rule@gmail.com>
2022-05-27 16:53:02 +0100

    Pass data into Views in EJS

--- app.js
+++ app.js
@@ -14,23 +14,29 @@ app.get('/', function (req, res) {
     // res.send() also infers the status code
     // res.send('Hello World')   //res.send() detects the Content-Type header and send the content
     // res.sendFile('./views/index.html', { root: __dirname})
-    res.render('index');   //rendering in ejs
+
+    const blogs = [
+        {title: "PSG signs Messi", snippet: "lorem lorem lorem lorem lorem lorem"},
+        {title: "Real Madrid win UCL", snippet: "lorem lorem lorem lorem lorem lorem"},
+        {title: "Messi wins WC for Argentina", snippet: "lorem lorem lorem lorem lorem lorem"},
+    ]
+    res.render('index', { title: "Blogger Gist", blogs });   //rendering in ejs
 })
 
 // listen to a GET request to about page
 app.get('/about', (req, res) => {
     // res.sendFile('./views/about.html', { root: __dirname})
-    res.render('about')
+    res.render('about', { title: "Blogger Gist" })
 })
 
 // redirects
 app.get('/blogs/create', (req, res) => {
-    res.render('create')
+    res.render('create', { title: "Create a New Blog" })
 })
 
 // error page if path does not exist
 // app.use() is used to create middleware and fire middleware functions
 app.use((req, res) => {
     // res.status(404).sendFile('./views/404.html', { root: __dirname})
-    res.status(404).render('404')
+    res.status(404).render('404', { title: "404" })
 })
