01a72688510120904e68b96135c57cb3fa4230ec
rolandexplore93 <roland2rule@gmail.com>
2022-05-30 10:53:20 +0100

    Implement MVC

---
+++ controllers/blogController.js
@@ -0,0 +1,49 @@
+const Blog = require('../models/blog')
+
+const blogIndex = (req, res) => {
+    Blog.find().sort({ createdAt: -1})
+        .then(result => {
+            res.render('index', { title: 'All Blogs', blogs: result})
+        })
+        .catch( err => console.log(err))
+}
+
+const blogCreatePost = (req, res) => {
+    console.log(req.body);
+
+    // create a new blog instance
+    const blog = new Blog(req.body)
+    blog.save()
+        .then(result => {
+            res.redirect('/blogs')
+        })
+        .catch(err => console.log(err))
+}
+
+const blogCreateGet = (req, res) => {
+    res.render('create', { title: "Create a New Blog" })
+}
+
+const blogDetails = (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id).then(result => {
+        res.render('details', { title: "Blog Details", blog: result })
+    })
+    .catch(err => console.log(err))
+}
+
+const blogDelete = (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+    .then((result) => {
+        res.json({ redirect: '/blogs'})
+    })
+}
+
+module.exports = {
+    blogIndex,
+    blogCreatePost,
+    blogCreateGet,
+    blogDetails,
+    blogDelete
+}
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,53 +1,15 @@
 const express = require('express');
-const Blog = require('../models/blog');
-
+const blogController = require('../controllers/blogController')
 // create a new instance of a route
 const router = express.Router();
 
 // create blog routes to display all blogs from database. NB: redirect homepage to /blogs
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1})
-        .then(result => {
-            res.render('index', { title: 'All Blogs', blogs: result})
-        })
-        .catch( err => console.log(err))
-})
-
+router.get('/', blogController.blogIndex)
 // POST request to store data to the database
-router.post('/', (req, res) => {
-    console.log(req.body);
-
-    // create a new blog instance
-    const blog = new Blog(req.body)
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs')
-        })
-        .catch(err => console.log(err))
-})
-
-
-router.get('/create', (req, res) => {
-    res.render('create', { title: "Create a New Blog" })
-})
-
+router.post('/', blogController.blogCreatePost)
+router.get('/create', blogController.blogCreateGet)
 // single route
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id).then(result => {
-        res.render('details', { title: "Blog Details", blog: result })
-    })
-    .catch(err => console.log(err))
-
-})
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-    .then((result) => {
-        res.json({ redirect: '/blogs'})
-    })
-})
-
+router.get('/:id', blogController.blogDetails)
+router.delete('/:id', blogController.blogDelete)
 
 module.exports = router;
